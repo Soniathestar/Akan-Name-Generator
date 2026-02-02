a0052b8aa1b1f2db914a70cfa62b3d4308cd4c28
Sonia Ngaruiya <soniangaruiya@gmail.com>
2026-02-02 15:42:34 +0300

    add corresponding names to the days of the week

--- script.js
+++ script.js
@@ -18,5 +18,60 @@ if(gender === null){
     return( year % 4 === 0 && year % 100 !==0) || (year % 400 === 0)
  }
 
- 
+ function calcDayofWeek(day, month, year)
+ {
+    const CC = Math.floor(year/100);
+    const YY = year % 100;
+
+    const d = Math.floor(
+        (
+            (4 * CC - 2 * CC - 1) +
+            (5 * YY / 4) +
+            (26 *(month + 1) /10) +
+            day
+        ) % 7
+
+        return d;
+    )
+    function isValidDate(day, month, year)
+    {
+        const daysInMonth = [
+            31,
+            isLeapYear(year) ? 29:28,
+            31,
+            30,
+            31,
+            30,
+            31,
+            31,
+            30,
+            31,
+            30,
+            31,
+        ];
+    }
+
+    return(
+        Month >= 1 &&
+        month <= 12 &&
+        day>= 1 &&
+        day <= daysInMonth[month - 1]
+    );
+ }
+
+ const maleNames = [
+    "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw" , "Kofi" , "Kwame"
+ ];
+
+ const femaleNames = [
+    "Akosua" , "Adwoa","Abenaa", "Akua", "Yaa", "Afua", "Ama"
+ ];
+
+ const akanName = 
+ gender.value === "male"
+ ? maleNames[dayIndex]
+ : femaleNames[dayIndex];
+
+ document.g
+
 
